098c553527c17686c4f664e5a439bf7e3ac1f890
Roman <r@rsd.at>
2025-01-22 22:54:38 +0100

    tpm

---
+++ components/uart_tpm2/uart_tpm2.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cmath>  // Wenn du cmath direkt in deiner Komponente benutzt
+#include "esphome/core/component.h"
+#include "esphome/components/uart/uart.h"
+
+#include <vector>
+
+namespace esphome {
+namespace uart_tpm2 {
+
+class UARTTPM2 : public Component, public uart::UARTDevice {
+ public:
+  UARTTPM2(uart::UARTComponent *parent) : UARTDevice(parent) {}
+  void setup() override;
+  void loop() override;
+
+ private:
+  std::vector<char> current_packet_;
+  bool receiving_ = false;
+  static const int max_packet_size_ = 512 * 3; // 3 bytes per color (RGB)
+
+  void processTPM2Packet(const std::vector<char>& packet);
+  void resetReception();
+};
+
+} // namespace uart_tpm2
+} // namespace esphome
