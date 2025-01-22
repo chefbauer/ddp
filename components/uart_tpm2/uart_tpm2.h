a3886531fa09577c641e0728cf1c55d50c0510fc
Roman <r@rsd.at>
2025-01-22 22:57:07 +0100

    sdf#

--- components/uart_tpm2/uart_tpm2.h
+++ components/uart_tpm2/uart_tpm2.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <cmath>  // Wenn du cmath direkt in deiner Komponente benutzt
+##include <cmath>  // Wenn du cmath direkt in deiner Komponente benutzt
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
 
