ccd4cbbe3cf856dbf3269f5206fafe0b9301acec
Roman <r@rsd.at>
2025-01-24 00:04:51 +0100

    yxc# Please enter the commit message for your changes. Lines starting

--- components/uart_tpm2/__init__.py
+++ components/uart_tpm2/__init__.py
@@ -4,6 +4,9 @@ from esphome.components import uart
 # from esphome.const import CONF_ID
 # from esphome.core import coroutine
 
+# uart_tpm2_ns = cg.esphome_ns.namespace('uart_tpm2')
+UARTTPM2 = uart_tpm2_ns.class_('UARTTPM2', cg.Component, uart.UARTDevice)
+
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(UARTTPM2),
     cv.Required(uart.CONF_UART_ID): cv.use_id(uart.UARTComponent),
@@ -11,8 +14,6 @@ CONFIG_SCHEMA = cv.Schema({
 
 
 
-# uart_tpm2_ns = cg.esphome_ns.namespace('uart_tpm2')
-# UARTTPM2 = uart_tpm2_ns.class_('UARTTPM2', cg.Component, uart.UARTDevice)
 
 
 # @coroutine
