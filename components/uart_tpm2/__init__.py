f07dea7c89fbdfbbd1056795be9a257e494f2f5e
Roman <r@rsd.at>
2025-01-24 00:04:03 +0100

    sdfc

--- components/uart_tpm2/__init__.py
+++ components/uart_tpm2/__init__.py
@@ -1,16 +1,19 @@
-# import esphome.codegen as cg
-# import esphome.config_validation as cv
-# from esphome.components import uart
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart
 # from esphome.const import CONF_ID
 # from esphome.core import coroutine
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(UARTTPM2),
+    cv.Required(uart.CONF_UART_ID): cv.use_id(uart.UARTComponent),
+})
+
+
 
 # uart_tpm2_ns = cg.esphome_ns.namespace('uart_tpm2')
 # UARTTPM2 = uart_tpm2_ns.class_('UARTTPM2', cg.Component, uart.UARTDevice)
 
-# CONFIG_SCHEMA = cv.Schema({
-#     cv.GenerateID(): cv.declare_id(UARTTPM2),
-#     cv.Required(uart.CONF_UART_ID): cv.use_id(uart.UARTComponent),
-# })
 
 # @coroutine
 # def to_code(config):
