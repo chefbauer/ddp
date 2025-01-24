87512eee49833726b31ec4a992336875a04c8a39
Roman <r@rsd.at>
2025-01-24 16:05:39 +0100

    asd# Please enter the commit message for your changes. Lines starting

--- components/uart_tpm2/uart_tpm2.cpp
+++ components/uart_tpm2/uart_tpm2.cpp
@@ -1,3 +1,13 @@
+#include "uart_tpm2.h"
+
+namespace esphome {
+namespace uart_tpm2 {
+
+void UARTTPM2::setup() {
+  // Starte den Stream, falls nötig
+  resetReception();
+}
+
 void UARTTPM2::loop() 
 {
     while (available()) 
@@ -48,3 +58,26 @@ void UARTTPM2::loop()
         }
     }
 }
+
+void UARTTPM2::processTPM2Packet(const std::vector<char>& packet) {
+  int data_index = 0;
+  for (int i = 0; i < std::min((int)packet.size() / 3, 450); ++i) {
+    if (data_index + 2 < packet.size()) {
+      // Speichere die empfangenen Farbdaten in die interne Variable
+      it_bg[i][0] = packet[data_index];
+      it_bg[i][1] = packet[data_index + 1];
+      it_bg[i][2] = packet[data_index + 2];
+      data_index += 3;
+    }
+  }
+  // Logge die Anzahl der verarbeiteten Farben
+  ESP_LOGD("uart_tpm2", "Processed %d colors", data_index / 3);
+}
+
+void UARTTPM2::resetReception() {
+  current_packet_.clear();
+  receiving_ = false;
+}
+
+}  // namespace uart_tpm2
+}  // namespace esphome
