a1d5b1a9a587f40d8a1039640f871667cb83eaa2
Roman <r@rsd.at>
2025-01-24 12:53:42 +0100

    asdasd

--- components/uart_tpm2/uart_tpm2.h
+++ components/uart_tpm2/uart_tpm2.h
@@ -2,13 +2,14 @@
 
 #include "esphome.h"
 #include <vector>
+#include "esphome/components/light/addressable_light.h"  // Hier wird Color definiert
 
 namespace esphome {
 namespace uart_tpm2 {
 
 class UARTTPM2 : public Component, public uart::UARTDevice {
  public:
-  UARTTPM2(uart::UARTComponent *parent, Color* bg) : UARTDevice(parent), it_bg_(bg) {}
+  UARTTPM2(uart::UARTComponent *parent, light::ESPColor* bg) : UARTDevice(parent), it_bg_(bg) {}
   void setup() override;
   void loop() override;
   void start();
@@ -19,7 +20,7 @@ class UARTTPM2 : public Component, public uart::UARTDevice {
   bool receiving_ = false;
   bool stopped_ = false;  // Flag für den Stopp-Zustand
   static const int max_packet_size_ = 512 * 3; // 3 bytes per color (RGB)
-  Color* it_bg_;  // Pointer auf die globale Variable
+  light::ESPColor* it_bg_;  // Pointer auf die globale Variable, nutze jetzt ESPColor
 
   void processTPM2Packet(const std::vector<char>& packet);
   void resetReception();
