802efaf370931b145b4e1ba25e518d051f988832
Roman <r@rsd.at>
2025-01-24 12:18:02 +0100

    asd

--- components/uart_tpm2/uart_tpm2.h
+++ components/uart_tpm2/uart_tpm2.h
@@ -9,10 +9,13 @@ class UARTTPM2 : public Component {
  public:
   void setup() override;
   void loop() override;
+  void start();
+  void stop();
 
  private:
   std::vector<char> current_packet_;
   bool receiving_ = false;
+  bool stopped_ = false;  // Flag für den Stopp-Zustand
   static const int max_packet_size_ = 512 * 3; // 3 bytes per color (RGB)
 
   void processTPM2Packet(const std::vector<char>& packet);
