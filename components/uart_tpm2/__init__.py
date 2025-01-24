2c158127cf60a10e26994862ffee360ef3c27a4a
Roman <r@rsd.at>
2025-01-24 15:07:10 +0100

    asd#

--- components/uart_tpm2/__init__.py
+++ components/uart_tpm2/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_NAME, CONF_UART_ID
 from esphome.core import coroutine
 
 uart_tpm2_ns = cg.esphome_ns.namespace('uart_tpm2')
