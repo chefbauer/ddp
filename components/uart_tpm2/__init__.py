92d99525c714220a36522dd21369994c14ec38ab
Roman <r@rsd.at>
2025-01-24 12:48:36 +0100

    asd

--- components/uart_tpm2/__init__.py
+++ components/uart_tpm2/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_UUID
 from esphome.core import coroutine
 
 uart_tpm2_ns = cg.esphome_ns.namespace('uart_tpm2')
@@ -10,10 +10,11 @@ UARTTPM2 = uart_tpm2_ns.class_('UARTTPM2', cg.Component, uart.UARTDevice)
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(UARTTPM2),
     cv.Required(uart.CONF_UART_ID): cv.use_id(uart.UARTComponent),
+    cv.Required(CONF_UUID): cv.use_id(cg.float_),
 })
 
 @coroutine
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = cg.new_Pvariable(config[CONF_ID], id(config[CONF_UUID]))
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
--- components/uart_tpm2/uart_tpm2.h
+++ components/uart_tpm2/uart_tpm2.h
@@ -8,7 +8,7 @@ namespace uart_tpm2 {
 
 class UARTTPM2 : public Component, public uart::UARTDevice {
  public:
-  UARTTPM2(uart::UARTComponent *parent) : UARTDevice(parent) {}
+  UARTTPM2(uart::UARTComponent *parent, Color* bg) : UARTDevice(parent), it_bg_(bg) {}
   void setup() override;
   void loop() override;
   void start();
@@ -19,6 +19,7 @@ class UARTTPM2 : public Component, public uart::UARTDevice {
   bool receiving_ = false;
   bool stopped_ = false;  // Flag für den Stopp-Zustand
   static const int max_packet_size_ = 512 * 3; // 3 bytes per color (RGB)
+  Color* it_bg_;  // Pointer auf die globale Variable
 
   void processTPM2Packet(const std::vector<char>& packet);
   void resetReception();
