564e4c47fc1047e2a9e073d962826bc88f01ad99
Roman <r@rsd.at>
2025-01-25 13:07:48 +0100

    asd

--- components/uart_tpm2/uart_tpm2.h
+++ components/uart_tpm2/uart_tpm2.h
@@ -20,6 +20,7 @@ class UARTTPM2 : public Component, public uart::UARTDevice {
   static const int max_packet_size_ = 1350; // 450 * 3 bytes per color (RGB)
   unsigned char it_intern_[1350]; // Interne Variable als unsigned char Array
   uint32_t last_log_time_ = 0; // Zeitstempel für das letzte Log
+  uint32_t loop_start_time_ = 0; // Zeitstempel Start
   uint32_t frames_processed_ = 0; // Anzahl der verarbeiteten Frames
   uint32_t frames_dropped_ = 0; // Anzahl der verworfenen Frames
 
