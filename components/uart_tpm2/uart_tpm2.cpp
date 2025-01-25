ddbe80998cac9d728528e9563e63623651e08d3c
Roman <r@rsd.at>
2025-01-25 17:24:41 +0100

    asd

--- components/uart_tpm2/uart_tpm2.cpp
+++ components/uart_tpm2/uart_tpm2.cpp
@@ -124,7 +124,7 @@ void UARTTPM2::loop()
         if (read_success)
         {
             written_bytes = fifo.write(buffer, buffer_size);
-            ESP_LOGI("uart_tpm2", "UART Lesezeit: %u msec | Buffer gelesen: %u", millis() - loop_start_time_, buffer_size);
+            //ESP_LOGI("uart_tpm2", "UART Lesezeit: %u msec | Buffer gelesen: %u", millis() - loop_start_time_, buffer_size);
         }
         // Debug: Logge die geschriebenen Bytes
         //ESP_LOGD("uart_tpm2", "Geschrieben: %u Bytes", written_bytes);
