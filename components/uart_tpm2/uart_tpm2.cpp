a8e1e636cca18e6ddeda323ab722913c11bfb314
Roman <r@rsd.at>
2025-01-25 03:34:39 +0100

    sdf

--- components/uart_tpm2/uart_tpm2.cpp
+++ components/uart_tpm2/uart_tpm2.cpp
@@ -29,11 +29,6 @@ void UARTTPM2::loop()
                 {
                     uint16_t data_size = (current_packet_[2] << 8) | current_packet_[3];
                     uint16_t expected_size = 2 + 2 + data_size + 1; // Header(2) + Paketgröße(2) + Daten(data_size) + Endbyte(1)
-                    
-                    if (available() < data_size)
-                    {
-                      return;
-                    }
 
                     if (current_packet_.size() >= expected_size) // Paket vollständig oder mehr Daten verfügbar
                     {
@@ -82,10 +77,6 @@ void UARTTPM2::loop()
                     else
                     {
                         // Paket ist noch nicht vollständig, warten wir
-                        if (millis() - loop_start_time >= 4) // 4ms Timeout für die Schleife
-                        {
-                            //return; // Abbrechen und beim nächsten Durchlauf fortsetzen
-                        }
                     }
                 } 
                 else 
