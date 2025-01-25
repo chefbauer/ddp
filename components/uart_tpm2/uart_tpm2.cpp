4ad076942a5d387a0eee01bc1e6c9aba75a55e3e
Roman <r@rsd.at>
2025-01-25 01:55:22 +0100

    asd

--- components/uart_tpm2/uart_tpm2.cpp
+++ components/uart_tpm2/uart_tpm2.cpp
@@ -153,7 +153,7 @@ void UARTTPM2::log_frame_stats() {
 
 void UARTTPM2::get_one_tpm2_package() {
     write(0x4C); // Sende das Zeichen 0x4C per UART
-    ESP_LOGI("uart_tpm2", "Gesendet: 0x4C");
+    //ESP_LOGI("uart_tpm2", "Gesendet: 0x4C");
 }
 
 }  // namespace uart_tpm2
