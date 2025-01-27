ab349fd542d27777490d288837f32fbca9d7406e
Roman <r@rsd.at>
2025-01-27 10:39:39 +0100

    asd

--- components/uart_tpm2/fifo_buffer.h
+++ components/uart_tpm2/fifo_buffer.h
@@ -3,7 +3,6 @@
 
 #include <array>
 #include <cstddef>
-#include <memory>
 
 template<size_t MaxSize>
 class FIFOBuffer {
@@ -14,7 +13,7 @@ private:
     size_t size = 0;
 
 public:
-    FIFOBuffer() : buffer{} {}
+    FIFOBuffer() : buffer{} {} // Alle Elemente initialisieren
     size_t write(const unsigned char* data, size_t len);
     size_t read(unsigned char* data, size_t len);
     unsigned char read();
@@ -25,5 +24,100 @@ public:
     void advanceReadPos(size_t amount);
     void clear();
 };
+
+// Alle Methoden müssen im Header definiert werden, da dies ein Template ist
+template<size_t MaxSize>
+size_t FIFOBuffer<MaxSize>::write(const unsigned char* data, size_t len) {
+    if (data == nullptr) {
+        throw std::invalid_argument("data pointer is null");
+    }
+
+    size_t bytes_to_write = std::min(len, MaxSize - size);
+    
+    for (size_t i = 0; i < bytes_to_write; ++i) {
+        buffer[write_pos] = data[i];
+        write_pos = (write_pos + 1) % MaxSize;
+    }
+    size += bytes_to_write;
+    return bytes_to_write;
+}
+
+template<size_t MaxSize>
+size_t FIFOBuffer<MaxSize>::read(unsigned char* data, size_t len) {
+    if (data == nullptr) {
+        throw std::invalid_argument("data pointer is null");
+    }
+
+    size_t bytes_to_read = std::min(len, size);
+
+    if (bytes_to_read > 0) {
+        if (read_pos + bytes_to_read <= MaxSize) {
+            std::memcpy(data, &buffer[read_pos], bytes_to_read);
+            std::memset(&buffer[read_pos], 0, bytes_to_read);
+        } else {
+            size_t first_part = MaxSize - read_pos;
+            std::memcpy(data, &buffer[read_pos], first_part);
+            std::memcpy(data + first_part, &buffer[0], bytes_to_read - first_part);
+            std::memset(&buffer[read_pos], 0, first_part);
+            std::memset(&buffer[0], 0, bytes_to_read - first_part);
+        }
+        read_pos = (read_pos + bytes_to_read) % MaxSize;
+        size -= bytes_to_read;
+    }
+    return bytes_to_read;
+}
+
+template<size_t MaxSize>
+unsigned char FIFOBuffer<MaxSize>::read() {
+    if (size == 0) {
+        throw std::out_of_range("Buffer is empty");
+    }
+    
+    unsigned char byte = buffer[read_pos];
+    buffer[read_pos] = 0;
+    read_pos = (read_pos + 1) % MaxSize;
+    size--;
+    return byte;
+}
+
+template<size_t MaxSize>
+bool FIFOBuffer<MaxSize>::available() const {
+    return size > 0;
+}
+
+template<size_t MaxSize>
+size_t FIFOBuffer<MaxSize>::getSize() const {
+    return size;
+}
+
+template<size_t MaxSize>
+size_t FIFOBuffer<MaxSize>::getMaxSize() const {
+    return MaxSize;
+}
+
+template<size_t MaxSize>
+unsigned char FIFOBuffer<MaxSize>::readAt(size_t index) const {
+    if (index >= size) {
+        throw std::out_of_range("Index out of range");
+    }
+    return buffer[(read_pos + index) % MaxSize];
+}
+
+template<size_t MaxSize>
+void FIFOBuffer<MaxSize>::advanceReadPos(size_t amount) {
+    if (amount > size) {
+        throw std::out_of_range("Cannot advance read position beyond buffer size");
+    }
+    read_pos = (read_pos + amount) % MaxSize;
+    size -= amount;
+}
+
+template<size_t MaxSize>
+void FIFOBuffer<MaxSize>::clear() {
+    read_pos = 0;
+    write_pos = 0;
+    size = 0;
+    std::memset(buffer.data(), 0, MaxSize);
+}
 
 #endif // FIFO_BUFFER_H
